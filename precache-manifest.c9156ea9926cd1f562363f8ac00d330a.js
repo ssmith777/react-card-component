bc774d111c3b12eae66a43b2c3248fd339d00727
ssmith <shawn.t.smith.777@gmail.com>
2019-09-02 22:20:12 -0500

    Updates

---
+++ precache-manifest.c9156ea9926cd1f562363f8ac00d330a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6f173e5cb59c6059f03deaad9889474",
+    "url": "/react-card-component/index.html"
+  },
+  {
+    "revision": "0814c44ee0f4fcaa0d92",
+    "url": "/react-card-component/static/css/main.729b72f4.chunk.css"
+  },
+  {
+    "revision": "4294c426290e9cb3fee0",
+    "url": "/react-card-component/static/js/2.fd2b56de.chunk.js"
+  },
+  {
+    "revision": "0814c44ee0f4fcaa0d92",
+    "url": "/react-card-component/static/js/main.82e6e202.chunk.js"
+  },
+  {
+    "revision": "e199b6a03adb3aea2e4e",
+    "url": "/react-card-component/static/js/runtime~main.f2b2091f.js"
+  }
+]);
